52b0c32422e1724fb80b5f2e598ad12124c2679d
Hosain <cryptichasan925@gmail.com>
2024-12-14 23:39:56 +0600

    get new accessToken

--- src/services/Auth/index.ts
+++ src/services/Auth/index.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 "use server";
 
 import axiosInstance from "@/src/lib/AxiousInstance";
@@ -72,5 +73,25 @@ export const getCurrentUser = async () => {
     };
   } else {
     return decodedToken;
+  }
+};
+
+
+export const getNewAccessToken = async () => {
+  try {
+    const refreshToken = (await cookies()).get("refreshToken")?.value;
+
+    const res = await axiosInstance({
+      url: "/auth/refresh-token",
+      method: "POST",
+      withCredentials: true,
+      headers: {
+        cookie: `refreshToken=${refreshToken}`,
+      },
+    });
+
+    return res.data;
+  } catch (error) {
+    throw new Error("Failed to get new access token");
   }
 };
