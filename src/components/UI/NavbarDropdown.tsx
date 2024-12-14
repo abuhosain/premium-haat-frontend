e5b5f7259b6c89400404a16f8ea8aeea27993e29
Hosain <cryptichasan925@gmail.com>
2024-12-14 23:36:53 +0600

    navbar dropdown setup done

--- src/components/UI/NavbarDropdown.tsx
+++ src/components/UI/NavbarDropdown.tsx
@@ -1,43 +1,64 @@
-"use client"; 
+import { protectedRoutes } from "@/src/constant";
+import { useUser } from "@/src/context/user.provider";
 import { logOut } from "@/src/services/Auth";
-import { Avatar } from "@nextui-org/avatar";
 import {
+  Avatar,
   Dropdown,
   DropdownItem,
   DropdownMenu,
   DropdownTrigger,
-} from "@nextui-org/dropdown";
-import { useRouter } from "next/navigation";
+} from "@nextui-org/react"; // Import Badge component
+import { usePathname, useRouter } from "next/navigation";
 
-const NavbarDropdown = () => {
+export default function NavbarDropdown() {
   const router = useRouter();
-  const handleNavigation = (pathname: string) => {
-    router.push(pathname);
+  const pathname = usePathname();
+  const { user, setIsLoading: userLoading } = useUser();
+
+  const handleLogout = () => {
+    logOut();
+    userLoading(true);
+
+    if (protectedRoutes.some((route: any) => pathname.match(route))) {
+      router.push("/");
+    }
   };
+
+  const handleNavigation = (pathname: string) => {
+    const pathnames = pathname.toLowerCase();
+    router.push(pathnames);
+  };
+
   return (
-    <div>
-      <Dropdown>
-        <DropdownTrigger>
-          <Avatar className="cursor-pointer" name="Hosain" />
-        </DropdownTrigger>
-        <DropdownMenu aria-label="Static Actions">
-          <DropdownItem key="new">Dashboard</DropdownItem>
-          <DropdownItem onClick={() => handleNavigation("/profile/settings")} key={"a"}>
-            Settings
-          </DropdownItem>
+    <Dropdown>
+      <DropdownTrigger>
+        <div className="flex items-center cursor-pointer relative">
+          <Avatar src={user?.img} />
+        </div>
+      </DropdownTrigger>
+      <DropdownMenu aria-label="Static Actions">
+        <DropdownItem
+          onClick={() => handleNavigation(`/${user?.role}`)}
+          key={"user_role"}
+        >
+          Dashboard
+        </DropdownItem>
+        <DropdownItem
+          onClick={() => handleNavigation("/profile/settings")}
+          key={"profile_settings"}
+        >
+          Settings
+        </DropdownItem>
 
-          <DropdownItem
-            key="delete"
-            className="text-danger"
-            color="danger"
-            onClick={() => logOut()}
-          >
-            Logout
-          </DropdownItem>
-        </DropdownMenu>
-      </Dropdown>
-    </div>
+        <DropdownItem
+          key="delete"
+          className="text-danger"
+          color="danger"
+          onClick={() => handleLogout()}
+        >
+          Logout
+        </DropdownItem>
+      </DropdownMenu>
+    </Dropdown>
   );
-};
-
-export default NavbarDropdown;
+}
